5e3daaf3418ae98b77beb7a679bd7e1614b8d9d9
shivakumar <shivakumar1342@gmail.com>
2020-10-13 16:44:00 +0530

    intial commit

---
+++ chatbot/src/ActionProvider.js
@@ -0,0 +1,33 @@
+class ActionProvider {
+    constructor(createChatBotMessage, setStateFunc) {
+      this.createChatBotMessage = createChatBotMessage;
+      this.setState = setStateFunc;
+    }
+  
+    // new method
+    greet() {
+      const greetingMessage = this.createChatBotMessage("Hi, friend.");
+      this.updateChatbotState(greetingMessage);
+    }
+  
+    handleJavascriptList = () => {
+      const message = this.createChatBotMessage(
+        "Fantastic, I've got the following resources for you on Javascript:",
+        {
+          widget: "javascriptLinks",
+        }
+      );
+  
+      this.updateChatbotState(message);
+    };
+  
+    updateChatbotState(message) {
+  
+      this.setState((prevState) => ({
+        ...prevState,
+        messages: [...prevState.messages, message],
+      }));
+    }
+  }
+  
+  export default ActionProvider;
